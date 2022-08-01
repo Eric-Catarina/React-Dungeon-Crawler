81f5506bced6b019921fc5723fb19d769c1052c9
Eric-Catarina <2020954200@teiacoltec.org>
2022-08-01 20:35:46 -0300

    Porque esse trem não funciona

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from 'react';
+
+function geraIcone(indice) {
+  switch (indice) {
+    case '0':
+      return <h1><i class="fa-solid fa-dungeon fa-10x text-light"></i></h1>;
+    case '1':
+      return <h1><i class="fa-solid fa-khanda fa-10x text-light"></i></h1>;
+    case '2':
+      return <h1><i class="fa-solid fa-dragon fa-10x text-light"></i></h1>;
+    case '3':
+      return <h1><i class="fa-solid fa-skull-crossbones fa-10x text-light"></i></h1>;
+  }
+}
+
+let iconeAtual = 0
+
+function IconePrincipal() {
+
+  const [contadorDeCliques, setaIndiceIcone] = useState(iconeAtual);
+
+  return (
+    <div>
+
+<a href='#' onClick={() => setaIndiceIcone(contadorDeCliques + 1)}>
+{geraIcone(contadorDeCliques)}
+              </a>
+      
+      Voce clicou {contadorDeCliques} vezes
+
+    </div>
+  );
+}
 
 function App() {
   return (
     <>
-    <div className='tudo pt-5 bg-secondary bg-gradient'>
-      <div className='container text-center containerCentral bg-dark bg-gradient d-flex align-items-center justify-content-center'>
-        <div className='row'>
-          <div className='col'>
-            <h1><i class="fa-solid fa-dungeon fa-10x text-light"></i></h1>
+      <div className='tudo pt-5 bg-secondary bg-gradient '>
+        <div className='shadow-lg border  border-5 border-dark rounded-circle container text-center containerCentral bg-dark bg-gradient d-flex align-items-center justify-content-center'>
+          <div className='row'>
+            <div className='col'>
+              {IconePrincipal()}
+             
+
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
     </>
   )
