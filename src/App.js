584b4f23668269609850352ccb58cce505c794c1
Eric-Catarina <2020954200@teiacoltec.org>
2022-08-03 23:27:45 -0300

    Todos Audios Funcionando

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from 'react';
 import "./App.scss"
 import BolhaCentral from "./components/BolhaCentral";
 
-import audioPortaSfx from "./audio/PortaRangendoSfx.wav"
-import audioArmaSfx from "./audio/EspadaSfx.mp3"
-import audioDragonSfx from "./audio/DragonSfx.mp3"
-import audioDeathSfx from "./audio/DeathSfx.mp3"
-import audioWinSfx from "./audio/WinSfx.wav"
 
 
 
 function App() {
  
-  const tocaSom = (indice) =>{
-    
-    let vetorAudios = []
-    let vetorAudiosSfx = [audioPortaSfx, audioArmaSfx ,audioDragonSfx ,audioDeathSfx ,audioWinSfx]
-
-    for (let audioAtual of vetorAudiosSfx){
-      vetorAudios.push(new Audio(audioAtual))
-    }
-
-    
-    
-      vetorAudios[indice].play()
-      
-
-  }
+  
  
   return (
     
     <>
     
-    {tocaSom(0)}
+    
       <div className='tudo fundoFundo '>
         <BolhaCentral
         />
--- src/components/Icone.js
+++ src/components/Icone.js
@@ -1,74 +1,97 @@
-
 import React, { useState } from 'react';
+import audioPortaSfx from "../audio/PortaRangendoSfx.wav"
+import audioArmaSfx from "../audio/EspadaSfx.mp3"
+import audioDragonSfx from "../audio/DragonSfx.mp3"
+import audioDeathSfx from "../audio/DeathSfx.mp3"
+import audioWinSfx from "../audio/WinSfx.wav"
 
 export default function Icone(properties) {
-    const indiceParaClasse = ["porta", "arma", "dragao", "caveira", "replay"]
-  
+  const indiceParaClasse = ["porta", "arma", "dragao", "caveira", "replay"]
 
 
 
 
 
-function RetornaIcone(indice) {
-  switch (indice) {
-    case 0:
-      return <h1><i class="fa-solid fa-dungeon fa-10x porta"></i></h1>;
-    case 1:
-      return <h1><i class="fa-solid fa-khanda fa-10x arma"></i></h1>;
-    case 2:
-      return <h1><i class="fa-solid fa-dragon fa-10x dragao"></i></h1>;
-    case 3:
-      return <h1><i class="fa-solid fa-skull-crossbones fa-10x caveira"></i></h1>;
-    case 4:
-      return <h1><i class="fa-solid fa-arrow-rotate-left fa-10x replay"></i></h1>;
 
+  function RetornaIcone(indice) {
+    switch (indice) {
+      case 0:
+        return <h1><i class="fa-solid fa-dungeon fa-10x porta"></i></h1>;
+      case 1:
+        return <h1><i class="fa-solid fa-khanda fa-10x arma"></i></h1>;
+      case 2:
+        return <h1><i class="fa-solid fa-dragon fa-10x dragao"></i></h1>;
+      case 3:
+        return <h1><i class="fa-solid fa-skull-crossbones fa-10x caveira"></i></h1>;
+      case 4:
+        return <h1><i class="fa-solid fa-arrow-rotate-left fa-10x replay"></i></h1>;
+
+    }
   }
-}
 
 
-function geraTexto(indice) {
-  switch (indice) {
-    case 0:
-      return "Entre no Calabouço"
-    case 1:
-      return "Pegue a Arma ";
-    case 2:
-      return "Mate o Dragão";
-    case 3:
-      return "Dragão Assassinado !";
-    case 4:
-      return "Jogar de Novo";
+  function geraTexto(indice) {
+    switch (indice) {
+      case 0:
+        return "Entre no Calabouço"
+      case 1:
+        return "Pegue a Arma ";
+      case 2:
+        return "Mate o Dragão";
+      case 3:
+        return "Dragão Assassinado !";
+      case 4:
+        return "Jogar de Novo";
 
+    }
   }
-}
 
-let iconeAtual = 0
+  let iconeAtual = 0
 
-    function SpawnaIconePrincipal() {
+  function SpawnaIconePrincipal() {
+    const [contadorDeCliques, setaIndiceIcone] = useState(iconeAtual);
+    if (contadorDeCliques == 5) {
+      setaIndiceIcone(0)
+    }
+    const tocaSom = (indice) => {
 
-        const [contadorDeCliques, setaIndiceIcone] = useState(iconeAtual);
-        if (contadorDeCliques == 5) {
-          setaIndiceIcone(0)
-        }
-        return (
-          <div>
-            <h1 className={indiceParaClasse[contadorDeCliques]}>
-                {geraTexto(contadorDeCliques)}
-                </h1>
+      let vetorAudios = []
+      let vetorAudiosSfx = [audioPortaSfx, audioArmaSfx, audioDragonSfx, audioDeathSfx, audioWinSfx]
 
-            <a href='#' onClick={() => setaIndiceIcone(contadorDeCliques + 1)}>
-              {RetornaIcone(contadorDeCliques)}
-              {console.log(indiceParaClasse[contadorDeCliques])}
-            </a>
-          </div>
-        );
+      for (let audioAtual of vetorAudiosSfx) {
+        vetorAudios.push(new Audio(audioAtual))
       }
+      vetorAudios[indice].play()
+    }
+
+    function TocaSomETrocaIcone(contadorDeCliques) {
+      setaIndiceIcone(contadorDeCliques + 1)
+      tocaSom(contadorDeCliques)
+    }
+    
+
+
+    
+    return (
+      <div>
+        <h1 className={indiceParaClasse[contadorDeCliques]}>
+          {geraTexto(contadorDeCliques)}
+        </h1>
+
+        <a href='#' onClick={() => TocaSomETrocaIcone(contadorDeCliques)}>
+          {RetornaIcone(contadorDeCliques)}
+          {console.log(indiceParaClasse[contadorDeCliques])}
+        </a>
+      </div>
+    );
+  }
 
   return (
     <div className='row'>
-            <div className='col'>
-              {SpawnaIconePrincipal()}
-            </div>
-          </div>
+      <div className='col'>
+        {SpawnaIconePrincipal()}
+      </div>
+    </div>
   )
 }
+
