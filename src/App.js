65af6a594c67e5a273bdc2177451d9a9ef3c74b0
Eric-Catarina <2020954200@teiacoltec.org>
2022-08-03 21:45:39 -0300

    Som porta

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import React, { useState } from 'react';
 import "./App.scss"
 import BolhaCentral from "./components/BolhaCentral";
 
+import audioPortaSfx from "./audio/creaking-door-2.wav"
+
 function App() {
+  const tocaSom = () =>{
+    const audioPorta = new Audio(audioPortaSfx)
+   
+    audioPorta.play();
+  }
+ 
   return (
+    
     <>
+    {tocaSom()}
       <div className='tudo fundoFundo '>
         <BolhaCentral
         />
       </div>
-
-
       <div />
+      
     </>
+    
   )
 }
 
